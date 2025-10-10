8bd756c4d46b5806a931bf5b9d662197062ea091
Uzonyi <izmcir@inf.elte.hu>
2025-10-10 11:07:51 +0200

    Kártyák betöltése

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -7,16 +7,16 @@ using UnityEngine.SocialPlatforms.Impl;
 public class CardLoader : MonoBehaviour
 {
     public TextAsset cardFile; // Drag & drop ide a fájlt Unity-ben
+    public List<CardType> Cards = new List<CardType>();
 
-    public List<CardType> LoadCards()
+    public void LoadCards()
     {
-        List<CardType> cards = new List<CardType>();
         string[] lines = cardFile.text.Split('\n');
 
         foreach (string line in lines)
         {
-            string[] parts = line.Trim().Split(';');
-            if (parts.Length < 52)
+            string[] parts = line.Split(';');
+            if (parts.Length == 52)
             {
                 int[,] Matrix = new int[7, 7];
                 for (int i = 0; i < 49; i++)
@@ -28,17 +28,15 @@ public class CardLoader : MonoBehaviour
 
                 CardType card = new CardType(parts[0], int.Parse(parts[1]), int.Parse(parts[2]), Matrix);
 
-                cards.Add(card);
+                Cards.Add(card);
             }
         }
-
-        return cards;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadCards();
     }
 
     // Update is called once per frame
