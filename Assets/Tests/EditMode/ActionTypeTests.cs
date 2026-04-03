440cffc65db05722a4b1c39e3b586e7c57ed0a6c
Uzonyi <uzpeti@hotmail.com>
2026-04-03 17:30:00 +0200

    Edit tesztek készen vannak
    Kész van db edit teszt

--- Assets/Tests/EditMode/ActionTypeTests.cs
+++ Assets/Tests/EditMode/ActionTypeTests.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NUnit.Framework;
 
-public class ActionTypeTests : MonoBehaviour
+public class ActionTypeTests
 {
-    // Start is called before the first frame update
-    void Start()
+    [Test]
+    public void ActionType_TakePuzzle_HasCorrectValue()
     {
-        
+        Assert.AreEqual(0, (int)ActionType.TakePuzzle);
     }
 
-    // Update is called once per frame
-    void Update()
+    [Test]
+    public void ActionType_TakeElement_HasCorrectValue()
     {
-        
+        Assert.AreEqual(1, (int)ActionType.TakeElement);
+    }
+
+    [Test]
+    public void ActionType_UpgradeElement_HasCorrectValue()
+    {
+        Assert.AreEqual(2, (int)ActionType.UpgradeElement);
+    }
+
+    [Test]
+    public void ActionType_PlaceElement_HasCorrectValue()
+    {
+        Assert.AreEqual(3, (int)ActionType.PlaceElement);
+    }
+
+    [Test]
+    public void ActionType_MasterAction_HasCorrectValue()
+    {
+        Assert.AreEqual(4, (int)ActionType.MasterAction);
+    }
+
+    [Test]
+    public void ActionType_TotalCount_IsFive()
+    {
+        var values = System.Enum.GetValues(typeof(ActionType));
+        Assert.AreEqual(5, values.Length);
+    }
+
+    [Test]
+    public void ActionType_CanBeCastFromInt()
+    {
+        ActionType action = (ActionType)3;
+        Assert.AreEqual(ActionType.PlaceElement, action);
+    }
+
+    [Test]
+    public void ActionType_CanBeCompared()
+    {
+        ActionType a = ActionType.TakePuzzle;
+        ActionType b = ActionType.TakePuzzle;
+        Assert.AreEqual(a, b);
     }
 }
--- Assets/Tests/EditMode/GameConfigTests.cs
+++ Assets/Tests/EditMode/GameConfigTests.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NUnit.Framework;
 
-public class GameConfigTests : MonoBehaviour
+public class GameConfigTests
 {
-    // Start is called before the first frame update
-    void Start()
+    [SetUp]
+    public void ResetDefaults()
     {
-        
+        GameConfig.PlayerCount = 2;
+        GameConfig.ThinkingTime = 30f;
+        GameConfig.PlayerNames = new string[] { "Játékos 1", "Játékos 2", "Játékos 3", "Játékos 4" };
+        GameConfig.PlayerColors = new Color[]
+        {
+            new Color(0.8f, 0.2f, 0.2f),
+            new Color(0.2f, 0.4f, 0.8f),
+            new Color(0.2f, 0.7f, 0.3f),
+            new Color(0.9f, 0.7f, 0.1f)
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    [Test]
+    public void GameConfig_DefaultPlayerCount_Is2()
     {
-        
+        Assert.AreEqual(2, GameConfig.PlayerCount);
+    }
+
+    [Test]
+    public void GameConfig_DefaultThinkingTime_Is30()
+    {
+        Assert.AreEqual(30f, GameConfig.ThinkingTime);
+    }
+
+    [Test]
+    public void GameConfig_DefaultPlayerNames_AreCorrect()
+    {
+        Assert.AreEqual("Játékos 1", GameConfig.PlayerNames[0]);
+        Assert.AreEqual("Játékos 2", GameConfig.PlayerNames[1]);
+        Assert.AreEqual("Játékos 3", GameConfig.PlayerNames[2]);
+        Assert.AreEqual("Játékos 4", GameConfig.PlayerNames[3]);
+    }
+
+    [Test]
+    public void GameConfig_PlayerCount_CanBeChanged()
+    {
+        GameConfig.PlayerCount = 4;
+        Assert.AreEqual(4, GameConfig.PlayerCount);
+    }
+
+    [Test]
+    public void GameConfig_ThinkingTime_CanBeChanged()
+    {
+        GameConfig.ThinkingTime = 60f;
+        Assert.AreEqual(60f, GameConfig.ThinkingTime);
+    }
+
+    [Test]
+    public void GameConfig_PlayerNames_CanBeChanged()
+    {
+        GameConfig.PlayerNames[0] = "Alice";
+        Assert.AreEqual("Alice", GameConfig.PlayerNames[0]);
+    }
+
+    [Test]
+    public void GameConfig_PlayerColors_HasFourEntries()
+    {
+        Assert.AreEqual(4, GameConfig.PlayerColors.Length);
+    }
+
+    [Test]
+    public void GameConfig_PlayerColors_CanBeChanged()
+    {
+        GameConfig.PlayerColors[0] = Color.blue;
+        Assert.AreEqual(Color.blue, GameConfig.PlayerColors[0]);
     }
 }
